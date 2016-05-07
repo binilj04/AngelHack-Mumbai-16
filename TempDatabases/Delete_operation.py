403d2d9974ea5cf4f3a4c8400a80f69c4553a536
mohan08p <mohanpawary1990@gmail.com>
2016-05-07 13:41:35 +0000

    Database files added

---
+++ TempDatabases/Delete_operation.py
@@ -0,0 +1,25 @@
+#__author__ = 'mohan08p'
+#To change this template use Tools | Templates.
+#!/usr/bin/python
+
+import MySQLdb
+
+# Open database connection
+db = MySQLdb.connect("localhost","root","codio","TESTDB" )
+
+# prepare a cursor object using cursor() method
+cursor = db.cursor()
+
+# Prepare SQL query to DELETE required records
+sql = "DELETE FROM EMPLOYEE WHERE AGE > '%d'" % (20)
+try:
+   # Execute the SQL command
+   cursor.execute(sql)
+   # Commit your changes in the database
+   db.commit()
+except:
+   # Rollback in case there is any error
+   db.rollback()
+
+# disconnect from server
+db.close()
